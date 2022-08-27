a8f99ab96a0439a2bf9762adebfa0afb590df007
Alexanderr23 <ar27109@gmail.com>
2022-08-27 19:12:22 -0400

    Odd NUmbers

---
+++ src/HW/oddNumbers.java
@@ -0,0 +1,11 @@
+package HW;
+
+public class oddNumbers {
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++){
+           if(i%2!=0){
+               System.out.println(i+"");
+           }
+        }
+    }
+}
